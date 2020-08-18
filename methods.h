8e528a971bce0d867d8b7d4f94212650dbd85d0b
ruent <ed784@nyu.edu>
2020-08-17 22:31:12 -0400

    some commit

--- methods.h
+++ methods.h
@@ -1,6 +1,21 @@
 //take a function f and solve for x in f(x) = 0
 //on the interval [a,b] where f(a) and f(b) has opposite signs
-#include <iostream>
+
+//in the main cpp, when I wrote includes like
+// <<<#include "nonlinsolver/methods.h", <<<#include "lgmarkov/model.h"
+// pragma once did not stop a redifinition error for these i line functions below
+//howvere old style ifndef here solved the issue
+//but still a little discipline in the "#include"s, a clear head about
+//their hierarchy is good I think
+
+//but pragma once works too!!!
+//At the moment I will be explicit about hierarchy
+//and turn them both off
+
+
+//#pragma once
+//#ifndef METHODS_H
+//#define METHODS_H 
 template<typename F>
 double inline bisectionMethod(const F& f, double a, double b, double tol)
 {
@@ -21,6 +36,7 @@ double inline bisectionMethod(const F& f, double a, double b, double tol)
         }
         mid = (left+right)/2;
         ymid  = f(mid);
+        //cout<< "x: "<<mid<<"value: "<<ymid<<"\n";
         
     }
     return mid;
@@ -37,3 +53,4 @@ struct example
 //quick test for bisection
 //example f;
 //double res = bisectionMethod(f, 0,2,0.0001);
+//#endif
